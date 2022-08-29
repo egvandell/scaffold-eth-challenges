c947925e7c22184ef6a7c8a2c85c5370a31ebaa9
Greg Van Dell <gvandell@engineation.com>
2022-08-29 15:45:41 -0400

    checkpoint 2 complete

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -1,11 +1,14 @@
-pragma solidity 0.8.4;
+pragma solidity >=0.8.4;
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
 
 contract YourToken is ERC20 {
+
+    address private FEaddress = 0x43c0A188E5E4312ef3614916d7d98e046D33188a;
+
     constructor() ERC20("Gold", "GLD") {
-        //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
+        _mint(FEaddress, 1000 * 10 ** 18);
     }
 }
